630791442b62354c9a235bdc3b130d41448b21c0
Yasmany Boffill <yboffill91@gmail.com>
2022-12-29 16:29:20 -0500

    Iniciando Script para app

---
+++ script.js
@@ -0,0 +1,10 @@
+const productos=[
+    {
+        id: 1,
+        nombre: "Movil GDM",
+        descripcion: "Movil de gama media GDM",
+        precio: 15000,
+        img: img.src='../src/product/movil1.jpg'
+    }
+    
+]

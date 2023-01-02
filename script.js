87f2ac21c1056179d3fbc5c5eb61ad7117aa090a
Yasmany Boffill <yboffill91@gmail.com>
2023-01-02 11:00:05 -0500

    Iniciando Carrito js

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ const productos=[
         id: 1,
         nombre: "Movil GDM",
         descripcion: "Movil de gama media GDM",
-        precio: 15000,
-        img: img.src='../src/product/movil1.jpg'
-    }
-    
+        precio: 16450,
+        img: './src/product/movil1.jpg',
+    },
+    {
+        id: 2,
+        nombre: "Movil Xiaomi",
+        descripcion: "Movil de gama media Xiaomi",
+        precio: 17000,
+        img: './src/product/movil2.jpg',
+    },   
 ]
+let carrito=[];
+productos.forEach((producto)=>{
+    let contenido=document.createElement("div");
+    contenido.innerHTML=
+})

3fa9db60dbb0934b4dcc0233fd717499b884029f
Yasmany Boffill <yboffill91@gmail.com>
2023-01-03 17:49:47 -0500

    finished

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const prodContenet = document.getElementById("prodContenet");
+const verCarrito = document.getElementById("verCarrito");
+const modal = document.getElementById("modal");
 const productos = [
   {
     id: 1,
@@ -97,18 +99,77 @@ productos.forEach((producto) => {
               <p class="descripcionProducto">${producto.descripcion}</p>
             </div>
             <div class="precio">&dollar; ${producto.precio}.00</div>
-            <div>
-              <button class="addCar" id="addCar">
-                <span> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" height="48" width="48">
-                  <path
-                    d="M14.35 43.95q-1.5 0-2.55-1.05-1.05-1.05-1.05-2.55 0-1.5 1.05-2.55 1.05-1.05 2.55-1.05 1.5 0 2.55 1.05 1.05 1.05 1.05 2.55 0 1.5-1.05 2.55-1.05 1.05-2.55 1.05Zm20 0q-1.5 0-2.55-1.05-1.05-1.05-1.05-2.55 0-1.5 1.05-2.55 1.05-1.05 2.55-1.05 1.5 0 2.55 1.05 1.05 1.05 1.05 2.55 0 1.5-1.05 2.55-1.05 1.05-2.55 1.05Zm-22.6-33 5.5 11.4h14.4l6.25-11.4Zm-1.5-3H39.7q1.15 0 1.75 1.05.6 1.05 0 2.1L34.7 23.25q-.55.95-1.425 1.525t-1.925.575H16.2l-2.8 5.2h24.55v3h-24.1q-2.1 0-3.025-1.4-.925-1.4.025-3.15l3.2-5.9L6.45 7h-3.9V4H8.4Zm7 14.4h14.4Z"
-                  />
-                </svg> </span>
-                A&ntilde;adir al carrito
-              </button>
-            </div>
           </div>
     `;
   prodContenet.append(contenido);
-  let comprar = document.getElementById("addCar");
+  let botonComprar = document.createElement("button");
+  botonComprar.innerHTML = `
+  <span>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" height="48" width="48"><path d="M23.25 17.35V11.2h-6.2v-3h6.2V2.05h3V8.2h6.15v3h-6.15v6.15ZM14.5 44q-1.5 0-2.55-1.05-1.05-1.05-1.05-2.55 0-1.5 1.05-2.55Q13 36.8 14.5 36.8q1.5 0 2.55 1.05 1.05 1.05 1.05 2.55 0 1.5-1.05 2.55Q16 44 14.5 44Zm20.2 0q-1.5 0-2.55-1.05-1.05-1.05-1.05-2.55 0-1.5 1.05-2.55 1.05-1.05 2.55-1.05 1.5 0 2.55 1.05 1.05 1.05 1.05 2.55 0 1.5-1.05 2.55Q36.2 44 34.7 44ZM14.5 33.65q-2.1 0-3.075-1.7-.975-1.7.025-3.45l3.05-5.55L7 7H3.1V4h5.8l8.5 18.2H32l7.8-14 2.6 1.4-7.65 13.85q-.45.85-1.225 1.3-.775.45-1.825.45h-15l-3.1 5.45h24.7v3Z"/></svg>
+</span>
+A&ntilde;adir al carrito
+  `;
+  contenido.append(botonComprar);
+  botonComprar.addEventListener("click", () => {
+    carrito.push({
+      id: producto.id,
+      nombre: producto.nombre,
+      precio: producto.precio,
+      img: producto.img,
+    });
+  });
+});
+verCarrito.addEventListener("click", () => {
+  modal.innerHTML="";
+  modal.style.display="flex";
+  const modalHeader = document.createElement("div");
+  modalHeader.className = "mHeader";
+  modalHeader.innerHTML = `
+  <h1>Carrito</h1>
+  <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" height="48" width="48"><path d="M14.35 43.95q-1.5 0-2.55-1.05-1.05-1.05-1.05-2.55 0-1.5 1.05-2.55 1.05-1.05 2.55-1.05 1.5 0 2.55 1.05 1.05 1.05 1.05 2.55 0 1.5-1.05 2.55-1.05 1.05-2.55 1.05Zm20 0q-1.5 0-2.55-1.05-1.05-1.05-1.05-2.55 0-1.5 1.05-2.55 1.05-1.05 2.55-1.05 1.5 0 2.55 1.05 1.05 1.05 1.05 2.55 0 1.5-1.05 2.55-1.05 1.05-2.55 1.05Zm-22.6-33 5.5 11.4h14.4l6.25-11.4Zm-1.5-3H39.7q1.15 0 1.75 1.05.6 1.05 0 2.1L34.7 23.25q-.55.95-1.425 1.525t-1.925.575H16.2l-2.8 5.2h24.55v3h-24.1q-2.1 0-3.025-1.4-.925-1.4.025-3.15l3.2-5.9L6.45 7h-3.9V4H8.4Zm7 14.4h14.4Z"/></svg></span>
+  `;
+  modal.append(modalHeader);
+  const cerrarCarrito = document.createElement("h1");
+  cerrarCarrito.innerText = "X";
+  cerrarCarrito.className = "cerrarCarrito";
+  cerrarCarrito.addEventListener("click", ()=>{
+    modal.style.display="none";
+  })
+  modalHeader.append(cerrarCarrito);
+  carrito.forEach((producto) => {
+    let carritoContent = document.createElement("div");
+    carritoContent.className = "modalContent";
+    carritoContent.innerHTML = `
+    <div class="modalLista">
+    <img src="${producto.img}">
+    <h3>${producto.nombre}</h3>
+    <h3>&dollar;${producto.precio}.00</h3>
+    </div>
+    <hr>
+    `;
+    modal.append(carritoContent);
+  });
+  const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+  
+  const totalPagar=document.createElement("div");
+  totalPagar.className="totales";
+  totalPagar.innerHTML=`
+  <h3>Monto Total: &dollar;${total}.00</h3>
+
+  `
+  let totalDesc=total-total/10;
+  const descuento=document.createElement("div");
+  descuento.innerHTML=`
+  <h3>Total a Pagar: &dollar;${totalDesc}.00</h3>
+  <h3>Ahorro de: &dollar;${total-totalDesc}.00</h3>
+  `
+  totalPagar.append(descuento);
+  modal.append(totalPagar);
+  const metPago=document.createElement("div");
+  metPago.className="metPago";
+  metPago.innerHTML=`
+  <button class="transfermovil"><img src="./src/icons/transfermovilico.png">Transfermovil</button>
+  <button class="enzona"><img src="./src/icons/enzonaico.png">EnZona</button>
+  `
+  modal.append(metPago);
 });

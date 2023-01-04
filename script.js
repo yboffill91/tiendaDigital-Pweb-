0990c42a8384b098a83a348883437f4a4cea3786
Yasmany Boffill <yboffill91@gmail.com>
2023-01-04 09:28:50 -0500

    retoques

--- script.js
+++ script.js
@@ -154,14 +154,14 @@ verCarrito.addEventListener("click", () => {
   const totalPagar=document.createElement("div");
   totalPagar.className="totales";
   totalPagar.innerHTML=`
-  <h3 class="pagar">Monto Total: &dollar;${total}.00</h3>
+  <h3 class="pagar">Monto Total: &dollar;&nbsp;${total}.00</h3>
 
   `
   let totalDesc=total-total/10;
   const descuento=document.createElement("div");
   descuento.innerHTML=`
-  <h3 class="totReal">Total a Pagar: &dollar;${totalDesc}.00</h3>
-  <h3 class="ahorro">Ahorro de: &dollar;${total-totalDesc}.00</h3>
+  <h3 class="totReal">Total a Pagar: &dollar;&nbsp;${totalDesc}.00</h3>
+  <h3 class="ahorro">Ahorro de: &dollar;&nbsp;${total-totalDesc}.00</h3>
   `
   totalPagar.append(descuento);
   modal.append(totalPagar);
